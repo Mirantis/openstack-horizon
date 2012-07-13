2ede175d6df70bac81295c7f6c7f4ed419a6d23f
Ilya Kharin <akscram@gmail.com>
2012-07-13 13:25:19 +0400

    Use balancerclient API.

--- horizon/api/balancer.py
+++ horizon/api/balancer.py
@@ -17,126 +17,136 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
-from __future__ import absolute_import
 
-import functools
 import logging
 import urlparse
 
 from django.utils.decorators import available_attrs
 
-from balancer import client as balancer_client
-from balancer.common import exception as balancer_exception
-from horizon.api.base import APIDictWrapper, url_for
+from balancerclient.v1 import client as balancer_client
+
+from horizon.api import base
+
+
+__all__ = ('lb_algorithms_get', 'lb_get', 'lb_list', 'lb_delete', 'lb_create',
+           'lb_update', 'lb_get_for_vm')
+
 
 LOG = logging.getLogger(__name__)
+LB_ALGORITHMS = ('RoundRobin', 'LeastConnections', 'LeastLoaded')
 
 
 def balancerclient(request):
-    o = urlparse.urlparse(url_for(request, 'image'))
-    #LOG.debug('balancer connection created for host "%s:%d"' %
-    #                       (o.hostname, o.port))
-    return balancer_client.Client("127.0.0.1",
-                                8181,
-                                auth_tok=request.user.token)
+    o = urlparse.urlparse(base.url_for(request, 'loadbalancer'))
+    url = "://".join((o.scheme, o.netloc))
+    LOG.debug('balancerclient connection created using token "%s" and url "%s"'
+              % (request.user.token.id, url))
+    return balancer_client.Client(endpoint=url, token=request.user.token.id)
 
 
-class LoadBalancer(APIDictWrapper):
-    """
-    Wrapper around glance image dictionary to make it object-like and provide
-    access to image properties.
-    """
-    _attrs = ['name', 'algorithm', 'protocol', 'trasport',
-             'status', 'created', 'id', 'updated',  'device_id',  'deployed',  'nodes']
-             
-    def set(self,  attrname,  value):
-        self._apidict[attrname] = value
+# LoadBalancer
 
-    def __getattr__(self, attrname):
-        if attrname == "properties":
-            if not hasattr(self, "_properties"):
-                properties_dict = super(LoadBalancer, self).__getattr__(attrname)
-              
-            return self._properties
-        else:
-            #LOG.debug('Called get attr on LoadBalancer: %s' % attrname)
-            return super(LoadBalancer, self).__getattr__(attrname)
-            
-class Node(APIDictWrapper):
-    """
-    Wrapper around glance image dictionary to make it object-like and provide
-    access to image properties.
-    """
-    _attrs = ['name', 'address', 'vm_instance', 'state','status', 
-             'id', 'deployed',  'nodes']
 
-    def __getattr__(self, attrname):
-        if attrname == "properties":
-            if not hasattr(self, "_properties"):
-                properties_dict = super(Node, self).__getattr__(attrname)
-              
-            return self._properties
-        else:
-            #LOG.debug('Called get attr on LoadBalancer: %s' % attrname)
-            return super(Node, self).__getattr__(attrname)
+def lb_algorithms_get(request):
+    return LB_ALGORITHMS
 
-def balancer_get_algorithms(request):
-    return balancerclient(request).get_algorithms()
-    
-def balancer_get_probes(request):
-     return balancerclient(request).get_probe_types()
 
-def balancer_get_loadbalancer(request,  lb_id):
-    
-    return LoadBalancer(balancerclient(request).get_loadbalancer(lb_id))
-    
-def balancer_get_loadbalancers(request):
-    lb_list = balancerclient(request).get_loadbalancers()
-    lbs = [LoadBalancer(lb) for lb in lb_list]
-    return lbs
+def lb_get(request, lb_id):
+    return balancerclient(request).loadbalancers.get(lb_id)
 
-def balancer_get_balancers_with_VM(request,  vm_id):
-    lb_list = balancerclient(request).get_balancers_with_vm(vm_id)
-    lbs = [LoadBalancer(lb) for lb in lb_list]
-    return lbs
-    
-def balancer_create_lb(request,  body):
-    return balancerclient(request).create_lb(body)
-    
-def balancer_get_nodes_of_lb(request,  lb_id):
-    node_list = balancerclient(request).get_nodes_for_lb(lb_id) 
-    nodes = [Node(node) for node in node_list]
-    return nodes
 
-def balancer_get_devices(request):
-    return balancerclient(request).get_devices()
+def lb_list(request):
+    return balancerclient(request).loadbalancers.list()
 
-def balancer_activate_vmnode_in_lbs(request,  node_id,  lb_id_list):
-    return balancerclient(request).activate_vmnode_in_lbs(node_id,  lb_id_list)
-    
-def balancer_suspend_vmnode_in_lbs(request,  node_id,  lb_id_list):
-    return balancerclient(request).suspend_vmnode_in_lbs(node_id,  lb_id_list)
-    
-def balancer_activate_vmnode(request,  node_id):
-    return balancerclient(request).activate_vmnode(node_id)
-    
-def balancer_suspend_vmnode(request,  node_id):
-    return balancerclient(request).suspend_vmnode(node_id)
-       
-def balancer_remove_vmnode_from_lbs(request,  node_id,  lb_id_list):
-     return balancerclient(request).remove_vmnode_from_lbs(node_id,  lb_id_list)
-    
-def balancer_add_vmnode_to_lb(request,  vmnode,  lb_id):
-    return balancerclient(request).add_vmnode_to_lb(vmnode,  lb_id)
-    
-def balancer_delete_lb(request,  lb_id):
-    return balancerclient(request).delete_lb(lb_id)
 
-def balancer_get_sticky_list(request):
-    return balancerclient(request).get_sticky_list()
+def lb_delete(request, lb_id):
+    balancerclient(request).loadbalancers.delete(lb_id)
 
-def balancer_add_probe_to_lb(request, probe,  lb_id):
-    return balancerclient(request).add_probe_to_lb(probe,  lb_id)
 
-def balancer_add_sticky_to_lb(request,  sticky,  lb_id):
-     return balancerclient(request).add_sticky_to_lb(sticky,  lb_id)
+def lb_create(request, name, algorithm, protocol, **extra):
+    return balancerclient(request).loadbalancers.create(name, algorithm,
+                                                        protocol, **extra)
+
+
+def lb_update(request, lb_id, name=None, algorithm=None, protocol=None,
+              **extra):
+    return balancerclient(request).loadbalancers.update(lb_id,
+                                                        name=name,
+                                                        algorithm=algorithm,
+                                                        protocol=protocol,
+                                                        **extra)
+
+
+def lb_get_for_vm(request, vm_id):
+    return balancerclient(request).loadbalancers.get_for_vm(vm_id)
+
+
+# Node
+
+
+def node_get(request, lb_id, node_id):
+    return balancerclient(request).nodes.get(lb_id, node_id)
+
+
+def node_list(request, lb_id):
+    return balancerclient(request).loadbalancers.list_nodes(lb_id)
+
+
+def node_delete(request, lb_id, node_id):
+    balancerclient(request).nodes.delete(lb_id, node_id)
+
+
+def node_create(request, lb_id, name, type, address, port, weight, status,
+                **extra):
+    return balancerclient(request).nodes.create(lb_id, name, type, address,
+                                                port, weight, status, **extra)
+
+
+def node_update(request, lb_id, node_id, name=None, type=None, address=None,
+                port=None, weight=None, status=None, **extra):
+    return balancerclient(request).nodes.update(lb_id, node_id,
+                                                name=name,
+                                                type=type,
+                                                address=address,
+                                                port=port,
+                                                weight=weight,
+                                                status=status,
+                                                **extra)
+
+
+# Probe
+
+
+def probe_get(request, lb_id, probe_id):
+    return balancerclient(request).probes.get(lb_id, probe_id)
+
+
+def probe_list(request, lb_id):
+    return balancerclient(request).loadbalancers.list_probes(lb_id)
+
+
+def probe_delete(request, lb_id, probe_id):
+    balancerclient(request).probes.delete(probe_id)
+
+
+def probe_create(request, lb_id, name, type, **extra):
+    return balancerclient(request).probes.create(lb_id, name, type, **extra)
+
+
+# Sticky
+
+
+def sticky_get(request, lb_id, sticky_id):
+    return balancerclient(request).stickies.get(lb_id, sticky_id)
+
+
+def sticky_list(request, lb_id):
+    return balancerclient(request).loadbalancers.list_stickies(lb_id)
+
+
+def sticky_delete(request, lb_id, sticky_id):
+    balancerclient(request).stickies.delete(lb_id, sticky_id)
+
+
+def sticky_create(request, lb_id, name, type, **extra):
+    return balancerclient(request).stickies.create(lb_id, name, type, **extra)
