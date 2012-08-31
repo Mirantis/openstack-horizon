558f167c85da9a6de55c0629cd07b3d8c8a7f140
denis <dviklov@mirantis.com>
2012-08-31 12:37:35 +0400

    initial commit for oslb-177

--- horizon/static/horizon/js/loadbalancing.js
+++ horizon/static/horizon/js/loadbalancing.js
@@ -23,3 +23,14 @@ horizon.addInitFunction(function() {
       evt.preventDefault();
     });
 });
+
+$(function(){
+    $('#instances :checkbox').change(function(){
+        console.log($('#instances :checked').length);
+        if ($('#instances :checked').length > 0){
+            $('a[id*="loadbalancing"]').removeClass('disabled');}
+        else{
+            $('a[id*="loadbalancing"]').addClass('disabled');
+        }    
+    })
+})
